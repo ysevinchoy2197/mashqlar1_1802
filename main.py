2b3b3add15402fa0c30841b1666cf30ad4a94ab4
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-02-18 15:25:46 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+#1-misol
+a = {
+
+}
+print(a)
+
+
+#2-misol
+talaba = {
+    "ism": "Ali",
+    "yosh": 20,
+    "shahar": "Toshkent"
+}
+print(talaba)
+
+#3-misol
+talaba = {
+    "ism": "Sardor",
+    "yosh": 22,
+    "shahar": "Samarqand"
+}
+print(talaba["ism"])
+
+
+#5-misol
+talaba = {
+    "ism": "Jasur",
+    "yosh": 21,
+    "shahar": "Namangan"
+}
+print(talaba)
+talaba["yosh"] = 23
+print(talaba)
